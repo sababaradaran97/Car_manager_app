4afb822c5c0ebf11df149b104a7beb52a0c76930
sababaradaran97 <sababaradaran97@gmail.com>
2025-09-15 18:26:07 +0330

    Update data_base_manager.py

--- data_base/data_base_manager.py
+++ data_base/data_base_manager.py
@@ -1,24 +1,13 @@
 import sqlite3
 
-Connection = sqlite3.connect( "data_base.db" )
-Cursor = Connection.cursor()
-Cursor.execute( 
-    """ 
-    CREATE table car_info (
-            id           integer primary key autoincrement,
-            name         text,
-            model        text,
-            color        text,
-            plate        text,
-            record_time  text
-     ) """ 
-    )
-
-Connection.commit()
-Connection.close()
-
-
 def save () :
     Connection = sqlite3.connect( "data_base.db" )
     Cursor = Connection.cursor()
-    Cursor.execute( """ INSERT INTO """ )
+    Cursor.execute(
+        """ INSERT INTO car_info
+                ( id, name, model, color, plate, record_time )
+                values ( {car_name} , {car_model} , {Car_color} , {car_plate} )
+        """ 
+        )
+    Connection.commit()
+    Connection.close()
