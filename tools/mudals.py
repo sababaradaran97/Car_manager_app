5cc7f411205ba4300a79ff1ec5f0cf3a79c00bd8
sababaradaran97 <sababaradaran97@gmail.com>
2025-09-15 18:27:46 +0330

    Update mudals.py

--- tools/mudals.py
+++ tools/mudals.py
@@ -3,4 +3,20 @@ from data_base.data_base_manager import *
 from validation import *
 
 def save_car () :
-    
+
+    try:
+        name_check( car_name.get() )
+        model_check( car_model.get() )
+
+        save()   
+
+        car_name.set( "" )
+        car_model.set( "" )
+        Car_color.set( "" )
+        car_plate.set( "" )
+
+    except Exception as e :
+        msg.showerror( "Error" , f"{e}" )
+
+
+   
